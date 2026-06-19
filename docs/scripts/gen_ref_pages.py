cac148c658dd4bb22cfcffa0c4c68c994d3c74b8
TeyAll <tey.berendschot@alliander.com>
2026-06-19 14:54:10 +0200

    docs: add reference for documentation generator script
    Signed-off-by: TeyAll <tey.berendschot@alliander.com>

--- docs/scripts/gen_ref_pages.py
+++ docs/scripts/gen_ref_pages.py
@@ -1,7 +1,10 @@
 # SPDX-FileCopyrightText: Contributors to the Cable Thermal Model project
 #
 # SPDX-License-Identifier: MPL-2.0
-"""Automatically generate the code for the API reference pages."""
+"""Automatically generate the code for the API reference pages.
+
+Inspired by the recipe on https://mkdocstrings.github.io/recipes/.
+"""
 
 import ast
 from pathlib import Path
